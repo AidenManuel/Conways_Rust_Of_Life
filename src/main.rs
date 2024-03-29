d423e21ffd38da3934d869cd254a93113cf073ee
Aiden Manuel <97690157+AidenManuel@users.noreply.github.com>
2024-03-29 14:09:33 -0300

    Got a cube which moves around the screen... Gonna try to draw up a grid next.

--- src/main.rs
+++ src/main.rs
@@ -11,36 +11,50 @@ use piston::window::WindowSettings;
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
+    rows: i32,
+    cols: i32,
+    x: usize,
+    y: usize
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
+        // Defining all necessary constants
+
         const WHITE: [f32; 4] = [255.0, 255.0, 255.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        const ROWS: usize = 20;
-        const COLS: usize = 20;
+        const ROWS: usize = 200;
+        const COLS: usize = 200;
+        const SIZE: i32 = 200;
+
+        self.rows = COLS as i32;
+        self.cols = ROWS as i32;
+
+        // Grids to store x and y coordinates of boxes
 
         let mut gridx: [f64; ROWS] = [0.0; ROWS];
         let mut gridy: [f64; COLS] = [0.0; COLS];
 
-        let mut i:usize = 0;
+        let mut i: usize = 0;
+        let x: usize = self.x;
+        let y: usize = self.y;
 
         while i < ROWS{
-            gridx[i] = i as f64;
+            gridx[i] = (i as f64) * ((SIZE / self.rows) as f64);
             i = i + 1;
         }
 
         i = 0;
 
         while i < COLS{
-            gridy[i] = i as f64;
+            gridy[i] = (i as f64) * ((SIZE / self.cols) as f64);
             i = i + 1;
         }
-
-        let _square = rectangle::square(0.0, 0.0, 50.0);
+        
+        let square = rectangle::square(gridx[x], gridy[y], 10.0);
         let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
 
         self.gl.draw(args.viewport(), |c, gl| {
@@ -52,18 +66,27 @@ impl App {
                 .trans(x, y)
                 .trans(-25.0, -25.0);
 
-            // Draw a box rotating around the middle of the screen.
-            for x in gridx{
-                for y in gridy{
-                    let square = rectangle::square(x, y, 1.0);
-                    rectangle(RED, square, transform, gl);
-                }
-            }
+            rectangle(RED, square, transform, gl);
         });
     }
 
     fn update(&mut self, _args: &UpdateArgs) {
-        
+        if (self.x as i32) < self.rows - 1
+
+        {
+            self.x += 1;
+        } 
+        else if (self.y as i32) < self.cols - 1 
+        {
+            self.y += 1;
+            self.x = 0;
+        } 
+        else 
+        {
+            self.y = 0;
+            self.x = 0;
+        }
+        println!("x = {0}\ny = {1}\n", self.x, self.y);
     }
 }
 
@@ -81,6 +104,10 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
+        rows: 0,
+        cols: 0,
+        x: 0,
+        y: 0
     };
 
     let mut events = Events::new(EventSettings::new());
